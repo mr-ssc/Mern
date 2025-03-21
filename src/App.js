fd831ca256ce6ef2cfa3ef421a7b662ad28c723e
mr-ssc <soyabchhuvara3@gmail.com>
2025-03-21 16:54:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './component/Home';
+import Signin from './component/Signin';
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    
+        <Routes>
+
+          <Route path='/Home' element={<Home/>}></Route>
+          <Route path='/' element={<Signin/>}></Route>
+        
+     
+       
+        </Routes>
+   
+
+
+
+    </>
+
   );
 }
 
-export default App;
+export default App
